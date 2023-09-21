a69a93e5dc439ad152f01bae549b28fdf9bccee4
Z Makwarela <39048926@mynwu.ac.za>
2023-09-21 11:05:06 +0200

    Updating my repository codes

---
+++ SuperStore P3/Repository/ProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data;
+using Models;
+
+namespace EcoPower_Logistics.Repository
+{
+    public class ProductRepository
+    {
+        protected readonly SuperStoreContext _context = new SuperStoreContext();
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products.ToList();
+        }
+    }
+}
